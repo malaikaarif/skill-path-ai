a8487d8d8a782abcff1d99542af00b213dea91c0
malaikaarif <malaikaarif905@gmail.com>
2026-05-31 19:04:50 +0500

    chore: Firebase initialized with FlutterFire CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/topic_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -47,7 +49,11 @@ final router = GoRouter(
   ],
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
